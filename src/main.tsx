33f1248375647eac455185f2c3cff69f33146f08
bhanurz <aswanggab@gmail.com>
2025-02-22 20:16:01 +0700

    feat: add browser router from react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 
 //ganti jadi strict mode lagi nanti
 createRoot(document.getElementById("root")!).render(
   <React.Fragment>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.Fragment>
 );
