9abe39d884d38ef14f8118b808f753527d8f598b
bhanurz <aswanggab@gmail.com>
2025-02-24 09:27:07 +0700

    feat: add home page route

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import FeaturedMovie from "@/components/FeaturedMovie";
+import HeroSection from "@/components/Hero";
+
+const Home = () => {
+  return (
+    <div>
+      <HeroSection />
+      <FeaturedMovie />
+    </div>
+  );
+};
+
+export default Home;
