4cd0d3c0529eb9e3ec140ef27e3c49079f651fa3
bhanurz <aswanggab@gmail.com>
2025-02-27 10:22:47 +0700

    fix: adjust page for home page

--- src/pages/Home/page.tsx
+++ src/pages/Home/page.tsx
@@ -1,12 +1,27 @@
-import FeaturedMovie from "@/pages/Home/components/FeaturedMovie";
-import HeroSection from "@/pages/Home/components/Hero";
+import SuspenseLoader from "@/components/SuspenseLoader";
+import { Suspense, lazy } from "react";
+
+const HeroSection = lazy(() => import("@/pages/Home/components/Hero"));
+const FeaturedMovie = lazy(
+  () => import("@/pages/Home/components/FeaturedMovie")
+);
 
 const Home = () => {
   return (
-    <div>
-      <HeroSection />
-      <FeaturedMovie />
-    </div>
+    <Suspense fallback={<SuspenseLoader />}>
+      <section id="hero-section" aria-labelledby="hero-heading">
+        <h2 id="hero-heading" className="sr-only">
+          Hero Section
+        </h2>
+        <HeroSection />
+      </section>
+      <section id="featured-movie" aria-labelledby="featured-heading">
+        <h2 id="featured-heading" className="sr-only">
+          Featured Movies
+        </h2>
+        <FeaturedMovie />
+      </section>
+    </Suspense>
   );
 };
 
