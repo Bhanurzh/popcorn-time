210b8fcfe57c0de8afbebd41a54733a1beadbc93
bhanurz <aswanggab@gmail.com>
2025-02-27 10:31:37 +0700

    feat: add formate date function

---
+++ src/utils/index.ts
@@ -0,0 +1,10 @@
+export const formatDate = (date: string) => {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  const formattedDate = new Date(date).toLocaleDateString("en-US", options);
+  return formattedDate;
+};
