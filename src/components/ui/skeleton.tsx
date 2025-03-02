7517bb5e8ddc8ad974912dfc823008e51e33c551
bhanurz <aswanggab@gmail.com>
2025-03-02 22:10:20 +0700

    feat: add ui skeleton components from shadcn

---
+++ src/components/ui/skeleton.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/lib/utils";
+
+function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="skeleton"
+      className={cn(
+        "bg-slate-400/10 animate-pulse rounded-md dark:bg-slate-50/10",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export { Skeleton };
