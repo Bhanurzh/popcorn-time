c529eca08da43448cd1dc2665d3c02c6726df30f
bhanurz <aswanggab@gmail.com>
2025-03-03 08:19:00 +0700

    feat: add film carousel component

---
+++ src/components/FilmCarousel.tsx
@@ -0,0 +1,81 @@
+import Autoplay from "embla-carousel-autoplay";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
+import { Film } from "@/types/shares";
+import FilmCard from "@/components/FilmCard";
+
+interface FilmCarouselProps {
+  films?: Film[];
+  carouselBasisItem: string;
+  isUsingCard: boolean;
+  activeItem?: number;
+  setActiveItem?: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const FilmCarousel: React.FC<FilmCarouselProps> = ({
+  films,
+  carouselBasisItem,
+  isUsingCard,
+  activeItem,
+  setActiveItem,
+}) => {
+  return (
+    <Carousel
+      plugins={[
+        Autoplay({
+          delay: 3000,
+          stopOnMouseEnter: true,
+          stopOnInteraction: false,
+        }),
+      ]}
+      opts={{
+        loop: true,
+        align: "center",
+        containScroll: "trimSnaps",
+        slidesToScroll: 1,
+      }}
+      className="w-full"
+    >
+      <CarouselContent>
+        {films?.map((film, index) =>
+          isUsingCard ? (
+            <CarouselItem key={index} className={carouselBasisItem}>
+              <FilmCard film={film} />
+            </CarouselItem>
+          ) : (
+            <CarouselItem
+              key={index}
+              className={carouselBasisItem}
+              onClick={() => setActiveItem && setActiveItem(index)}
+            >
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${film.poster_path}`}
+                srcSet={`
+                  https://image.tmdb.org/t/p/w300/${film.poster_path} 300w,
+                  https://image.tmdb.org/t/p/w500/${film.poster_path} 500w,
+                  https://image.tmdb.org/t/p/w780/${film.poster_path} 780w
+                `}
+                alt={film.title || film?.name}
+                width={160}
+                height={250}
+                className={`md:w-[160px] w-[120px] md:h-[250px] rounded-lg object-cover ${
+                  activeItem === index ? "border-[3px] border-red-primary" : ""
+                }`}
+                loading="lazy"
+              />
+            </CarouselItem>
+          )
+        )}
+      </CarouselContent>
+      <CarouselPrevious className="absolute left-0 top-1/2 transform -translate-y-1/2 text-white p-2 bg-red-primary hover:text-red-primary hover:bg-white transition duration-200 hover:scale-110 border-none" />
+      <CarouselNext className="absolute right-0 top-1/2 transform -translate-y-1/2 text-white p-2 bg-red-primary hover:text-red-primary hover:bg-white transition duration-200 hover:scale-110 border-none" />
+    </Carousel>
+  );
+};
+
+export default FilmCarousel;
