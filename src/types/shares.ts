e3dec95d1f75e0c0d241b09eae0f8fd5caa1b0ad
bhanurz <aswanggab@gmail.com>
2025-03-08 20:47:26 +0700

    style: add missing semicolon

--- src/types/shares.ts
+++ src/types/shares.ts
@@ -62,18 +62,23 @@ export interface TrailerResult {
 }
 
 export interface Review {
-  author: string
-  author_details: AuthorDetails
-  content: string
-  created_at: string
-  id: string
-  updated_at: string
-  url: string
+  author: string;
+  author_details: AuthorDetails;
+  content: string;
+  created_at: string;
+  id: string;
+  updated_at: string;
+  url: string;
 }
 
 export interface AuthorDetails {
-  name: string
-  username: string
-  avatar_path?: string
-  rating: number
+  name: string;
+  username: string;
+  avatar_path?: string;
+  rating: number;
+}
+
+export interface Genre {
+  id: number;
+  name: string;
 }
