209e408ca7d539bb2132ff6b949bb6ac8e23ce3e
bhanurz <aswanggab@gmail.com>
2025-03-09 16:26:25 +0700

    fix: adjust component for tv series page

--- src/pages/TvSeries/page.tsx
+++ src/pages/TvSeries/page.tsx
@@ -1,10 +1,14 @@
+import DropdownInput from "@/components/DropdownInput";
+import FilmHeader from "@/components/film/FilmHeader";
 import CustomPagination from "@/components/Pagination";
+import SearchInput from "@/components/SearchInput";
 import SuspenseLoader from "@/components/SuspenseLoader";
 import useGetFilms from "@/services/useGetFilms";
+import useGetGenreFilm from "@/services/useGetGenreFilm";
 import { FilmListResponse } from "@/types/apiResponse";
 import { lazy, Suspense, useState } from "react";
 
-const FilmList = lazy(() => import("@/components/FilmList"));
+const FilmList = lazy(() => import("@/components/film/FilmList"));
 
 const TvSeries = () => {
   const [searchTerm, setSearchTerm] = useState<string>("");
@@ -16,6 +20,7 @@ const TvSeries = () => {
     page,
     genreId
   );
+  const { data: genreOptions } = useGetGenreFilm("tv");
 
   const handlePageChange = (newPage: number) => {
     setPage(newPage);
@@ -24,18 +29,28 @@ const TvSeries = () => {
 
   return (
     <Suspense fallback={<SuspenseLoader />}>
-      <section id="movie-section" aria-labelledby="movie-heading">
+      <section
+        className="flex flex-col space-y-4 py-6 px-5 w-full"
+        id="movie-section"
+        aria-labelledby="movie-heading"
+      >
         <h2 id="movie-heading" className="sr-only">
           Tv Series Section
         </h2>
+        <FilmHeader title="Tv Series" isUseDefaultSearch={false}>
+          <SearchInput searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+          <DropdownInput
+            options={genreOptions}
+            onChange={setGenreId}
+            value={genreId}
+            optionPlaceHoler="Genre"
+            optionLabel="Select Genre"
+          />
+        </FilmHeader>
         <FilmList
           data={data as FilmListResponse}
           isLoading={isLoading}
           error={error}
-          searchTerm={searchTerm}
-          setSearchTerm={setSearchTerm}
-          genreId={genreId}
-          setGenreId={setGenreId}
         />
         <CustomPagination
           currentPage={page}
