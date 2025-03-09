4f3730e9932379231c677c0ffaf7d7864abb5fa5
bhanurz <aswanggab@gmail.com>
2025-03-09 16:26:56 +0700

    fix: adjust component for movie page

--- src/pages/Movie/page.tsx
+++ src/pages/Movie/page.tsx
@@ -1,10 +1,14 @@
+import DropdownInput from "@/components/DropdownInput";
+import FilmHeader from "@/components/film/FilmHeader";
 import CustomPagination from "@/components/Pagination";
+import SearchInput from "@/components/SearchInput";
 import SuspenseLoader from "@/components/SuspenseLoader";
 import useGetFilms from "@/services/useGetFilms";
+import useGetGenreFilm from "@/services/useGetGenreFilm";
 import { FilmListResponse } from "@/types/apiResponse";
 import { lazy, Suspense, useState } from "react";
 
-const FilmList = lazy(() => import("@/components/FilmList"));
+const FilmList = lazy(() => import("@/components/film/FilmList"));
 
 const Movie = () => {
   const [searchTerm, setSearchTerm] = useState<string>("");
@@ -16,6 +20,7 @@ const Movie = () => {
     page,
     genreId
   );
+  const { data: genreOptions } = useGetGenreFilm("movie");
 
   const handlePageChange = (newPage: number) => {
     setPage(newPage);
@@ -24,18 +29,28 @@ const Movie = () => {
 
   return (
     <Suspense fallback={<SuspenseLoader />}>
-      <section id="movie-section" aria-labelledby="movie-heading">
+      <section
+        className="flex flex-col space-y-4 py-6 px-5 w-full"
+        id="movie-section"
+        aria-labelledby="movie-heading"
+      >
         <h2 id="movie-heading" className="sr-only">
           Movie Section
         </h2>
+        <FilmHeader title="All Movies" isUseDefaultSearch={false}>
+          <SearchInput searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+          <DropdownInput
+            options={genreOptions}
+            value={genreId}
+            onChange={setGenreId}
+            optionLabel="Genre"
+            optionPlaceHoler="Select Genre"
+          />
+        </FilmHeader>
         <FilmList
           data={data as FilmListResponse}
           isLoading={isLoading}
           error={error}
-          searchTerm={searchTerm}
-          setSearchTerm={setSearchTerm}
-          genreId={genreId}
-          setGenreId={setGenreId}
         />
         <CustomPagination
           currentPage={page}
