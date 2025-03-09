5cf4de9b0f62688a1c3d3e62e78a5cd5ba5eca0e
bhanurz <aswanggab@gmail.com>
2025-03-09 16:27:21 +0700

    feat: add route for movie, tv series, and trending

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/page";
 import Detail from "./pages/Detail/page";
+import Movie from "./pages/Movie/page";
+import TvSeries from "./pages/TvSeries/page";
+import Trending from "./pages/Trending/page";
 import Layout from "./layout";
 import NotFound from "./pages/NotFound";
 
@@ -9,6 +12,9 @@ function App() {
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/trending" element={<Trending />} />
+        <Route path="/movie" element={<Movie />} />
+        <Route path="/tv-series" element={<TvSeries />} />
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="*" element={<NotFound />} />
       </Route>
